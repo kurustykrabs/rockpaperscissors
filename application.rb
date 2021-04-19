c58c81503ae18d834c45790c223e453b532810bc
root <root@KRISTY-PC.localdomain>
2021-04-19 16:45:45 +0800

    first commit

---
+++ application.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+
+class RockPaperScissors < Sinatra::Base
+
+enable :sessions
+
+  get '/' do
+    erb :layout
+  end
+
+  # post '/' do
+  #   session ['player_name'] = params [:player_name]
+  #   @player_name = session ['player_name']
+  #   erb :layout
+  # end
+
+
+end
