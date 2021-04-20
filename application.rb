e965544b3d9c208e74b327b5cea286ebbf0be49a
root <root@KRISTY-PC.localdomain>
2021-04-20 16:56:19 +0800

    Add menu, game

--- application.rb
+++ application.rb
@@ -5,14 +5,19 @@ class RockPaperScissors < Sinatra::Base
 enable :sessions
 
   get '/' do
-    erb :layout
+    erb :homepage
   end
 
   post '/' do
     session['player_name'] = params[:player_name]
     @player_name = session['player_name']
-    erb :layout
+    erb :menu
   end
 
-
+  get '/game' do
+    # session['results'] = []
+    # session ['count'] = 0
+    # @rounds = session['rounds'].to_i
+    erb :game
+  end
 end
