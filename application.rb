5e3a3b972482f89bf71e834dbc34de404ca58d7b
root <root@KRISTY-PC.localdomain>
2021-04-21 16:20:19 +0800

    update latest coding

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class RockPaperScissors < Sinatra::Base
 
-enable :sessions
+  enable :sessions
 
   get '/' do
     erb :homepage
@@ -11,13 +11,45 @@ enable :sessions
   post '/' do
     session['player_name'] = params[:player_name]
     @player_name = session['player_name']
-    erb :menu
+    erb :rounds
   end
 
   get '/game' do
-    # session['results'] = []
-    # session ['count'] = 0
-    # @rounds = session['rounds'].to_i
     erb :game
+  end
+
+  post '/rounds' do
+    session['rounds'] = params[:rounds]
+    @rounds = params['rounds']
+    erb :menu
+  end
+
+  get '/game/throw/:player_choice' do
+    session['count'] = 0
+    options = ['Rock','Paper','Scissors']
+    computer_choice = rand(options.length)
+    player_choice = params[:player_choice]
+    @computer_answer = options[computer_choice]
+    session['count'] += 1
+
+    @count = session['count']
+    @rounds = session['rounds'].to_i
+
+    if player_choice == 'Rock' && @computer_answer == 'Scissors'
+      @result = 'You win!'
+    elsif player_choice == 'Paper' && @computer_answer == 'Rock'
+      @result ='You win!'
+    elsif player_choice == 'Scissors' && @computer_answer == 'Paper'
+      @result = 'You win!'
+    elsif @computer_answer == 'Rock' && player_choice == 'Scissors'
+      @result = 'You lose :('
+    elsif @computer_answer == 'Paper' && player_choice == 'Rock'
+      @result = 'You lose :('
+    elsif @computer_answer == 'Scissors' && player_choice == 'Paper'
+      @result = 'You lose :('
+    else
+      @result = 'Tie!'
+    end
+      erb :game
   end
 end
